c0b1875418e4c7a9187b5de35e9ae8097e8731cb
Miquel Llabres Riera <miquelllabres@gmail.com>
2020-03-21 14:37:58 -0400

    Finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+//Keys.js - looks for the correct set of keys
+
+if ( process.env.NODE_ENV === 'production'){
+	//we are in production, return production keys;
+	module.exports = require('./prod');
+}else{
+	//we are in development, return development keys
+	module.exports = require('./dev');
+}
---
+++ config/prod.js
@@ -0,0 +1,7 @@
+//prod.js - production keys
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  database: process.env.DATABASE,
+  cookieKey: process.env.COOKIEKEY
+};
